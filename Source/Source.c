cfb03d04a8e53c6b74f0e0b3cad79014f553b13e
Pengyi Mu <mupengyi@foxmail.com>
2017-10-15 14:35:14 +0800

    Update Source.c

--- Source/Source.c
+++ Source/Source.c
@@ -1,4 +1,3 @@
-/*暂时未完成*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h> 
@@ -7,7 +6,7 @@
 #include <stdbool.h> 
 
 #define NUM 7
-#define TYPE 5
+#define TYPE 6
 
 /*****************************控制台颜色函数宏定义*****************************/
 #define RED SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED)
@@ -24,12 +23,15 @@
 
 char Map[NUM][NUM];
 
+unsigned int Score = 0;
+
 void Init(void);
 void Display(int,int,int,int,int,int); 
 char RandCh(void);
 void Master(void);
 int CheckLegal(int*,int*);
 int Eliminate(int*,int*);
+int CheckYes(void);
 int RemoveCh(void);
 int ShiftAndRand(void);
 
@@ -41,6 +43,9 @@ int main(void)
 	return 0;
 }
 
+/**
+*初始化Map 
+*/
 void Init(void)
 {
 	int index,index2;
@@ -51,8 +56,16 @@ void Init(void)
 			Map[index][index2] = RandCh();
 		}
 	}
+	/*检查有无可消除的*/
+	while(CheckYes())
+	{
+		RemoveCh();
+	}
 }
 
+/*
+*操作执行 
+*/
 void Master(void)
 {
 	char ch;
@@ -61,7 +74,7 @@ void Master(void)
 	int DisplayPosy[2] = {-1,-1};
 	int Posx,Posy;
 	Posx = Posy = 0;
-	Display(-1,-1,-1,-1,Posx,Posy);
+	Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 	while(1)
 	{
 		ch = getch();
@@ -74,7 +87,7 @@ void Master(void)
 				{
 					Posx--;
 				}
-				Display(-1,-1,-1,-1,Posx,Posy);
+				Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				break;
 			}
 			case 'D':
@@ -84,7 +97,7 @@ void Master(void)
 				{
 					Posx++;
 				}
-				Display(-1,-1,-1,-1,Posx,Posy);
+				Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				break;
 			}
 			case 'W':
@@ -94,7 +107,7 @@ void Master(void)
 				{
 					Posy--;
 				}
-				Display(-1,-1,-1,-1,Posx,Posy);
+				Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				break;
 			}
 			case 'S':
@@ -104,7 +117,7 @@ void Master(void)
 				{
 					Posy++;
 				}
-				Display(-1,-1,-1,-1,Posx,Posy);
+				Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				break;
 			}
 			case 'I':
@@ -113,14 +126,7 @@ void Master(void)
 				DisplayPosx[KeyNum%2] = Posx;
 				DisplayPosy[KeyNum%2] = Posy;
 				KeyNum++;
-				if(CheckLegal(DisplayPosx,DisplayPosy))
-				{
-					Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
-				}
-				else
-				{
-					Display(-1,-1,-1,-1,Posx,Posy);
-				}
+				Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				break;
 			}
 			case 'O':
@@ -129,25 +135,70 @@ void Master(void)
 				if(CheckLegal(DisplayPosx,DisplayPosy))
 				{
 					Eliminate(DisplayPosx,DisplayPosy);
+					while(CheckYes())
+					{
+						RemoveCh();
+					}
 					Display(DisplayPosx[0],DisplayPosx[1],DisplayPosy[0],DisplayPosy[1],Posx,Posy);
 				}
-			} 
+			}
 		}
 	}
 }
 
+/**
+*检测是否存在可消除的组合.
+*@return 存在返回true,不存在返回false 
+*/
+int CheckYes(void)
+{
+	int index,index2;
+	for(index = 0;index < NUM;index++)
+	{
+		for(index2 = 0;index2 < NUM;index2++)
+		{
+			if(Map[index][index2] == Map[index][index2 + 1] && Map[index][index2 + 1] == Map[index][index2 + 2])
+			{
+				return true;
+			}
+			else if(Map[index][index2] == Map[index + 1][index2] && Map[index + 1][index2] == Map[index + 2][index2])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+/**
+*交换选择元素，启动消除函数 
+*/
 int Eliminate(int* X,int*Y)
 {
 	char Tmp;
-	Map[X[0]][Y[0]] = Tmp;
+	
+	Tmp = Map[X[0]][Y[0]];
 	Map[X[0]][Y[0]] = Map[X[1]][Y[1]];
 	Map[X[1]][Y[1]] = Tmp;
-	RemoveCh();
+	Score = Score + (Tmp = RemoveCh());
+	/*如果没什么可消除的*/
+	if(!Tmp)
+	{
+		Tmp = Map[X[0]][Y[0]];
+		Map[X[0]][Y[0]] = Map[X[1]][Y[1]];
+		Map[X[1]][Y[1]] = Tmp;
+	}
+	return 0;
 }
 
+/**
+*将可消除的排列组合归为0
+*@return 返回消除元素个数. 
+*/
 int RemoveCh(void)
 {
 	int x,y;
+	int ScoreTimes = 0;
 	bool MapList[NUM][NUM] = {{false}};
 	
 	for(x = 0;x < NUM;x++)
@@ -157,27 +208,21 @@ int RemoveCh(void)
 			if(Map[x][y] == Map[x][y+1] && Map[x][y+1] == Map[x][y+2])
 			{
 				int Tmp = 2;
-				int Each;
+				MapList[x][y] = MapList[x][y+1] = MapList[x][y+2] = true;
 				while(Map[x][y+Tmp] == Map[x][y+Tmp+1])
 				{
+					MapList[x][y+Tmp+1] = true;
 					Tmp++;
-				}
-				for(Each = 0;Each <= Tmp;Each++)
-				{
-					MapList[x][y+Each] = true;
 				}
 			}
 			if(Map[x][y] == Map[x+1][y] && Map[x+1][y] == Map[x+2][y])
 			{
 				int Tmp = 2;
-				int Each;
+				MapList[x][y] = MapList[x+1][y] = MapList[x+2][y] = true;
 				while(Map[x+Tmp][y] == Map[x+Tmp+1][y])
 				{
+					MapList[x+Tmp+1][y] = true;
 					Tmp++;
-				}
-				for(Each = 0;Each <= Tmp;Each++)
-				{
-					MapList[x+Each][y] = true;
 				}
 			} 
 		} 
@@ -190,33 +235,37 @@ int RemoveCh(void)
 			if(MapList[x][y])
 			{
 				Map[x][y] = 0;
+				ScoreTimes++;
 			}
 		}
 	}
 	
 	ShiftAndRand();
+	return ScoreTimes;
 	
 }
 
+/**
+*将Map内由于RemoveCh()造成的空白进行填充. 
+*/
 int ShiftAndRand(void)
 {
 	int index,index2;
 	int Tmp;
-	for(index = NUM-1;index >= 0;index--)
-	{
-		for(index2 = NUM-1;index2 >= 0;index2--)
+	int Number = 0;
+	while(Number < 7)
+	{ 
+		for(index = 0;index < NUM;index++)
 		{
-			if(Map[index][index2] == 0)
+			for(index2 = NUM-1;index2 >= 0;index2--)
 			{
-				while(Map[index][index2] == 0)
+				if(Map[index][index2] == 0)
 				{
-					for(Tmp = index2;Tmp > 0; Tmp--)
-					{
-						Map[index][Tmp] = Map[index][Tmp-1]; 
-					}
+					Map[index][index2] = Map[index][index2-1];
 				}
 			}
 		}
+		Number++;
 	}
 	
 	for(index = 0;index < NUM;index++)
@@ -232,6 +281,9 @@ int ShiftAndRand(void)
 	return 0;
 }
 
+/**
+*检测Master函数内的DisplayPosx，DisplayPosx参数是否合法. 
+*/
 int CheckLegal(int * N1,int * N2)
 {
 	if(N1[0] != -1 && N1[1] != -1)
@@ -244,14 +296,20 @@ int CheckLegal(int * N1,int * N2)
 	return 0;
 }
 
+/**
+*将Map和Score打印出来. 
+*/
 void Display(int x1,int x2,int y1,int y2,int Fingerx,int Fingery)
 {
 	int x,y;
 	char Finger[NUM][NUM] = {{0}};
 	
-	if(x1 != -1)
+	if(x1 != -1 && y1 != -1)
 	{
 		Finger[x1][y1] = '.';
+	}
+	if(x2 != -1 && y2 != -1)
+	{
 		Finger[x2][y2] = '.';
 	}
 	
@@ -269,12 +327,17 @@ void Display(int x1,int x2,int y1,int y2,int Fingerx,int Fingery)
 			{
 				White;
 			} 
-			printf("%c%c",Map[x][y],Finger[x][y]);
+			printf(" %c%c ",Map[x][y],Finger[x][y]);
 		}
-		printf("\n");
+		printf("\n\n");
 	}
+	printf("\n Score:%u\n",Score);
 }
 
+/*
+*随机生成元素.
+*@return 返回生成元素的ASCII. 
+*/
 char RandCh(void)
 {
 	switch(rand()%TYPE)
@@ -283,6 +346,7 @@ char RandCh(void)
 		case 1:return '#';
 		case 2:return '$';
 		case 3:return '&';
-		case 4:return '%'; 
+		case 4:return '%';
+		case 5:return '*';
 	}
 } 
